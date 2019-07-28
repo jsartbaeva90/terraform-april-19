f962278cb948b481595d3e06bba21e66113a15d9
jsartbaeva <jsartbaeva91@gmail.com>
2019-07-28 17:14:59 -0500

    added updates

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web1" {
      ami = "ami-0bbc25e23a7640b9b"
      instance_type = "t2.micro"
      key_name = "${aws_key_pair.terraform_april.key_name}"
-     security_groups = [${"aws_security_group.sec1.name"}]
+     security_groups = ["${aws_security_group.sec1.name}"]
 
 
 
