e1e8e4a5bc0e592b95876143331f9af0bb80ad55
jsartbaeva <jsartbaeva91@gmail.com>
2019-07-28 16:34:35 -0500

    added sec group

---
+++ sec_group.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "sec1" {
+  name        = "sec1"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-1bccc87d"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "-1"
+    cidr_blocks = "0.0.0.0/0"
+  }
+}
