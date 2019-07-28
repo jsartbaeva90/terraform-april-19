0e03021619c3a2dfc9ae36d33d40f3ccc380616b
jsartbaeva <jsartbaeva91@gmail.com>
2019-07-28 16:52:41 -0500

    added tags

--- instance.tf
+++ instance.tf
@@ -3,4 +3,16 @@ resource "aws_instance" "web1" {
      ami = "ami-0bbc25e23a7640b9b"
      instance_type = "t2.micro"
      key_name = "terraform_april"
+
+
+
+
+
+     tags = {
+       Name = "Dev"
+       Dept = "IT"
+       Group = "April"
+       Created_by = "Zhazgul"
+  }
 }
+
