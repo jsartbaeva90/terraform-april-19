e624f2fcb719446101832098edb26e89337e7114
jsartbaeva <jsartbaeva91@gmail.com>
2019-07-28 16:02:38 -0500

    added

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
    region = "eu-west-1"
 }
 resource "aws_instance" "web1" {
-     ami = "0bbc25e23a7640b9b"
+     ami = "ami-0bbc25e23a7640b9b"
      instance_type = "t2.micro"
   
 }
