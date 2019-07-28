8cff886ed7596d64d7e4e4a20023d73691fe9f78
jsartbaeva <jsartbaeva91@gmail.com>
2019-07-28 17:02:51 -0500

    added sec.group

--- instance.tf
+++ instance.tf
@@ -2,8 +2,8 @@
 resource "aws_instance" "web1" {
      ami = "ami-0bbc25e23a7640b9b"
      instance_type = "t2.micro"
-     key_name = "terraform_april"
-
+     key_name = "${aws_key_pair.terraform_april}"
+     security_groups = [${aws_security_group.sec1.id}]
 
 
 
