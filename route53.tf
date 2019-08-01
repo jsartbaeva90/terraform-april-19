e46e365d73d7781f49692b24f34b0c47bdc6a0ba
jsartbaeva <jsartbaeva91@gmail.com>
2019-07-31 20:52:23 -0500

    added route53

---
+++ route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "jenkins" {
+  zone_id = "Z1LNH5KLN13PU2"
+  name    = "zhazgul.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.web1.public_ip}"]
+}
