bce4cc6ac4ce570a1183042cb8faf932c23f3e1c
jsartbaeva <jsartbaeva91@gmail.com>
2019-07-31 21:16:47 -0500

    update

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "jenkins" {
   zone_id = "Z1LNH5KLN13PU2"
-  name    = "zhazgul.com"
+  name    = "jenkins.zhazgul.com"
   type    = "A"
   ttl     = "300"
   records = ["${aws_instance.web1.public_ip}"]
