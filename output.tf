a54e8fdf64898bca45c0e392ffa1026c5abaf7f1
jsartbaeva <jsartbaeva91@gmail.com>
2019-07-31 19:24:41 -0500

    added just the output

--- output.tf
+++ output.tf
@@ -2,8 +2,16 @@ output "key_name" {
   value = "${aws_key_pair.terraform_april.key_name}"
 }
 
-
 output "bucketname" {
   value = "${aws_s3_bucket.b.bucket}"
 }
+output "ID" {
+  value = "${aws_instance.web1.id}"
+}
 
+output "username" {
+   value = "ec-user2"
+}
+output "public_ip" {
+   value = "${aws_instance.web1.public_ip}"
+}
