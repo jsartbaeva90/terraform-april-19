90ec21449f0549dbb1ddec68ca654efd8cc0ad34
jsartbaeva <jsartbaeva91@gmail.com>
2019-07-31 19:33:56 -0500

    addedd userdata

--- instance.tf
+++ instance.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "web1" {
      instance_type = "t2.micro"
      key_name = "${aws_key_pair.terraform_april.key_name}"
      security_groups = ["${aws_security_group.sec1.name}"]
+     user_data = "${file("userdata.sh")}"
 
 
 
